da53e2f6907018adb8449b3f55ff305e4054f62f
KanishkaMK <kanishkasudhakaran@gmail.com>
2023-09-12 13:34:22 +0530

    whatsapp clone app

---
+++ whatsappfl/lib/ui/screens/home_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/ui/pages/camera_page.dart';
+import 'package:whatsapp/ui/pages/chat_page.dart';
+import 'package:whatsapp/ui/pages/status_screen.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin{
+      late TabController _controller;
+      @override 
+      void initState(){
+        super.initState();
+        _controller = TabController(length: 4,initialIndex: 1, vsync:this);
+      }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: const Text('WhatsApp'),
+      actions: [
+        Row(
+          children: [
+            
+            IconButton(onPressed:() {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => Camera(),));
+          }, icon: Icon(Icons.camera_alt)),
+            // search icon
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+            // add space
+            const SizedBox(
+              width: 20.00,
+            ),
+            PopupMenuButton(onSelected:(value){
+              print(value);
+            }, itemBuilder: (BuildContext context){
+              return const [
+                 PopupMenuItem(value: 1, child: Text('New group')),
+                 PopupMenuItem(value: 2, child: Text('New broadcast')),
+                 PopupMenuItem(value: 3, child: Text('Linked devices')),
+                 PopupMenuItem(value: 4, child: Text('Starred messages')),
+                 PopupMenuItem(value: 5, child: Text('Payments')),
+                 PopupMenuItem(value: 6, child: Text('Settings')),
+              ];
+            })
+          ],
+        ),
+      ],
+
+      bottom: TabBar(
+        tabs: const [
+          Tab(
+            icon: Icon(
+              Icons.people,
+            ),
+          ),
+          Tab(
+            text: 'CHATS',
+          ),
+          Tab(
+            text: 'STATUS',
+          ),
+          Tab(
+            text: 'CALLS',
+          ),
+          ],
+          controller: _controller,
+      ),
+    ), 
+    body: TabBarView(
+      controller: _controller,
+      children: [
+        Text('Communities'),
+        ChatPage(),
+        StatusPage(),
+        Text('Calls'),
+      ],
+      ),
+    
+    );
+  }
+    }
